9047a813db491de2c8e53b5a1ec323f7daa7ac30
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-06-16 09:00:11 +0700

    Update inlinebot.py

--- Panda/core/inlinebot.py
+++ Panda/core/inlinebot.py
@@ -54,9 +54,7 @@ def ibuild_keyboard(buttons):
 
 
 def main_menu():
-    result = builder.photo(
     file=ilhammansiz,
-    link_preview=False,
     text = f"╔══════════════\n╠🐼 𝗛𝗲𝗹𝗽 𝗜𝗻𝗹𝗶𝗻𝗲 𝗕𝗼𝘁\n╠🐼 𝗣𝗮𝗻𝗱𝗮-𝗨𝘀𝗲𝗿𝗯𝗼𝘁\n╠🐼 𝗨𝘀𝗲𝗿 :{mention}\n╚══════════════🐼"
     buttons = [
         (
