c36bbf75353a02115690b65de1e0c586366a153a
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-06-16 15:50:44 +0700

    Update inlinebot.py

--- Panda/core/inlinebot.py
+++ Panda/core/inlinebot.py
@@ -534,6 +534,7 @@ async def inline_handler(event):  # sourcery no-metrics
 @check_owner
 async def on_plug_in_callback_query_handler(event):
     buttons = [
+        (Button.inline("❌ Hapus semua ❌", "❌ Terhapus ❌"),
         (Button.inline("🤖 Open Menu 🤖", data="mainmenu"),),
     ]
     await event.edit("👾 Menu Closed 👾", buttons=buttons)
