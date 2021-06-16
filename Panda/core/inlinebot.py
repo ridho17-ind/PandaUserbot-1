bd9a04d8701e1b142a937f3780db1ea77ea8e964
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-06-16 09:50:50 +0700

    Update inlinebot.py

--- Panda/core/inlinebot.py
+++ Panda/core/inlinebot.py
@@ -55,12 +55,12 @@ def ibuild_keyboard(buttons):
 
 
 def main_menu():
-    ilhammansiez,
+    {ilhammansiez},
     text = f"╔══════════════\n╠🐼 𝗛𝗲𝗹𝗽 𝗜𝗻𝗹𝗶𝗻𝗲 𝗕𝗼𝘁\n╠🐼 𝗣𝗮𝗻𝗱𝗮-𝗨𝘀𝗲𝗿𝗯𝗼𝘁\n╠🐼 𝗨𝘀𝗲𝗿 :{mention}\n╚══════════════🐼"
     buttons = [
         (
             Button.inline(
-                f"↪ GRUP SUPPORT",
+                f"🤖 GRUP SUPPORT 🤖",
                 data="ilhammansiz",
             ),
         ),
