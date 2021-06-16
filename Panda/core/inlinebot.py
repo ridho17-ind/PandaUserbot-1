589af3b61c59221c854c3b243d0d5089811cacce
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-06-16 10:20:47 +0700

    Update inlinebot.py

--- Panda/core/inlinebot.py
+++ Panda/core/inlinebot.py
@@ -55,9 +55,7 @@ def ibuild_keyboard(buttons):
 
 
 def main_menu():
-    PANDA_IMG = Config.ALIVE_PIC or None
-    if PANDA_IMG and PANDA_IMG.endswith((".jpg", ".png")):
-    PANDA_IMG,
+    logo = ilhammansiez
     text = f"╔══════════════\n╠🐼 𝗛𝗲𝗹𝗽 𝗜𝗻𝗹𝗶𝗻𝗲 𝗕𝗼𝘁\n╠🐼 𝗣𝗮𝗻𝗱𝗮-𝗨𝘀𝗲𝗿𝗯𝗼𝘁\n╠🐼 𝗨𝘀𝗲𝗿 :{mention}\n╚══════════════🐼"
     buttons = [
         (
